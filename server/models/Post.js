bb559d6659f8984def72e912fbc02074f7f692e7
snoopyods <snoopyods@gmail.com>
2023-10-05 14:51:45 -0700

    Added mock data to represent users

--- server/models/Post.js
+++ server/models/Post.js
@@ -3,28 +3,28 @@ import mongoose from "mongoose";
 const postSchema = mongoose.Schema(
     {
         userId: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         firstName: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         lastName: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         location: String,
         description: String,
         picturePath: String,
         userPicturePath: String,
         likes: {
-        type: Map,
-        of: Boolean,
+            type: Map,
+            of: Boolean,
         },
         comments: {
-        type: Array,
-        default: [],
+            type: Array,
+            default: [],
         },
     },
     { timestamps: true }
