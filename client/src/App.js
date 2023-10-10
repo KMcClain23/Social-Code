a9aa83921819cf8deffa9e68b69af8ebff578f07
snoopyods <snoopyods@gmail.com>
2023-10-10 16:42:40 -0700

    Added profile page, deleted fake users and posts, fixed adding/removing friend functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
@@ -15,18 +14,24 @@ function App() {
   const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-  <div className="app">
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes>
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
-        <Route path="/profile/:userId" element={isAuth ? <ProfilePage/> : <Navigate to="/" />} />
-      </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
-  </div>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
